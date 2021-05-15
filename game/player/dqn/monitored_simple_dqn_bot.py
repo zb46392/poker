b563c5933585b989a218d667412d05c0f2035338
zb46392 <zb46392@oss.unist.hr>
2021-05-15 16:20:54 +0200

    MonitoredSimpleDqnBot addition:
     - Saving state with best progress

--- game/player/dqn/monitored_simple_dqn_bot.py
+++ game/player/dqn/monitored_simple_dqn_bot.py
@@ -5,15 +5,20 @@ from typing import Dict, Optional
 
 class MonitoredSimpleDqnBot(SimpleDqnBot):
     MONITOR_FREQUENCY = 100
+    SHOULD_SAVE_BEST_AVERAGE = True
+    BEST_MODEL_NAME = 'best_model'
 
     def __init__(self, chips: int, name: Optional[str]) -> None:
         super().__init__(chips, name)
         self._average_win = 0
+        self._best_average = 0
         self._calculation_cnt = 0
         self._monitor_episode_cnt = 0
         self._monitor = Monitor(self._nn.model, self._generate_monitoring_comment_params())
         self._monitor_frequency = self.MONITOR_FREQUENCY
         self._is_monitoring = False
+        self._best_model_name = self.BEST_MODEL_NAME
+        self._should_save_best_average = self.SHOULD_SAVE_BEST_AVERAGE
 
     def activate_monitoring(self) -> None:
         self._is_monitoring = True
@@ -48,6 +53,10 @@ class MonitoredSimpleDqnBot(SimpleDqnBot):
                 if self._monitor_episode_cnt % self._monitor_frequency == 0:
                     self._monitor.update_progress(self._average_win)
                     self._monitor.monitor()
+
+                    if self._should_save_best_average and self._average_win > self._best_average:
+                        self.save_model(self._best_model_name)
+                        self._best_average = self._average_win
 
     def _reset_average_calculation(self) -> None:
         self._average_win = 0
